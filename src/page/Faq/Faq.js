e1a6bfa2e882ff5c6c8cbba3d8912ace0d8dd4fd
Mydulislam <mydulislam693852@gmail.com>
2022-10-26 22:58:05 +0600

    FAQ page quistions inserted

--- src/page/Faq/Faq.js
+++ src/page/Faq/Faq.js
@@ -2,8 +2,40 @@ import React from 'react';
 
 const Faq = () => {
     return (
-        <div>
-            <h1>This is faq pag</h1>
+        <div className='text-justify'>
+            <h1 className='fs-1 fw-bold text-decoration-underline text-center mb-4'>Frequently Asked Question(FAQ)</h1>
+            <h3>Question: What are some of the advantages of this courses?</h3>
+            <b>Ans:</b>
+            <ul>
+                <li><b>Personalized learning:</b> Not all students learn the same way. Web-based learning allows instructors to deliver the same content using different media, like videos or simulations, personalizing learning. Online classes providing round-the-clock access to materials and lectures also let students study when they feel most focused and engaged.</li>
+                <li><b>Accessibility:</b> Online programs transcend time, geographic, and other barriers to higher education. This can be helpful for those who work full-time, live in remote regions, or serve in the military.</li>
+                <li><b>Adaptability:</b> Learning management systems that integrate text-to-speech and other adaptive technologies support learners with physical, behavioral, and learning challenges.</li>
+                <li><b>Engagement:</b> Studies show online students tend to achieve the same learning results in half the time as classroom-based students.</li>
+            </ul>
+            <h3>Question: How does online education work on a day-to-day basis?</h3>
+            <b>Ans:</b>
+            <p>Instructional methods, course requirements, and learning technologies can vary significantly from one online program to the next, but the vast bulk of them use a learning management system (LMS) to deliver lectures and materials, monitor student progress, assess comprehension, and accept student work. LMS providers design these platforms to accommodate a multitude of instructor needs and preferences. While some courses deliver live lectures using video conferencing tools, others allow students to download pre-recorded lectures and use message boards to discuss topics. Instructors may also incorporate simulations, games, and other engagement-boosters to enhance learning. Students should research individual programs to find out how and when they would report to class; how lectures and materials are delivered; how and how much they would collaborate with faculty and peers; and other important details. We address many of these instructional methods and LMS capabilities elsewhere in this guide.</p>
+            <h3>Question: Is online education as effective as face-to-face instruction?</h3>
+            <b>Ans:</b>
+            <p>Online education may seem relatively new, but years of research suggests it can be just as effective as traditional coursework, and often more so. According to a U.S. Department of Education analysis of more than 1,000 learning studies, online students tend to outperform classroom-based students across most disciplines and demographics. Another major review published the same year found that online students had the advantage 70 percent of the time, a gap authors projected would only widen as programs and technologies evolve.While these reports list several plausible reasons students might learn more effectively online—that they have more control over their studies, or more opportunities for reflection—medium is only one of many factors that influence outcomes. Successful online students tend to be organized self-starters who can complete their work without reporting to a traditional classroom. Learning styles and preferences matter, too. Prospective students should research programs carefully to identify which ones offer the best chance of success.</p>
+            <h3>Question: How do I know if online education is right for me?</h3>
+            <b>Ans:</b>
+            <p>Choosing the right degree program takes time and careful research no matter how one intends to study. Learning styles, goals, and programs always vary, but students considering online colleges must consider technical skills, ability to self-motivate, and other factors specific to the medium. A number of colleges and universities have developed assessments to help prospective students determine whether they are prepared for online learning. You can access a compilation of assessments from many different colleges online. Online course demos and trials can also be helpful, particularly if they are offered by schools of interest. Students can call online colleges and ask to speak an admissions representative who can clarify additional requirements and expectations.</p>
+            <h3>Question: What technical skills do online students need?</h3>
+            <b>Ans:</b>
+            <p>Online learning platforms are typically designed to be as user-friendly as possible: intuitive controls, clear instructions, and tutorials guide students through new tasks. However, students still need basic computer skills to access and navigate these programs. These skills include: using a keyboard and a mouse; running computer programs; using the Internet; sending and receiving email; using word processing programs; and using forums and other collaborative tools. Most online programs publish such requirements on their websites. If not, an admissions adviser can help.</p>
+            <h3>Question: Will I get credit for this course?</h3>
+            <b>Ans:</b>
+            <p>Online Learning credit courses are comparable to on-campus courses and appear on your University of Minnesota Morris transcript in exactly the same manner as an on-campus course. You will receive University credit for these courses.</p>
+            <h3>Question: What does the course number mean?</h3>
+            <b>Ans:</b>
+            <p>The course number designates the level of instruction for which the course is intended. One thousand numbered courses are designed as introductory, general enrichment offerings. Unless otherwise specified, they require no previous background in the subject. Most of the Online Learning courses meet general learning and elective requirements for an undergraduate degree. Three and four thousand level courses offer more advanced study and often have prerequisites.</p>
+            <h3>Question: Can I find out more about these courses?</h3>
+            <b>Ans:</b>
+            <p>The courses are detailed in the course offerings. For further information, please contact Online Learning.</p>
+            <h3>Question: What happens if I don't finish the course?</h3>
+            <b>Ans:</b>
+            <p>If you do not finish the course during the semester enrollment period, and if you do not request an incomplete, the course will appear on your University of Minnesota transcript with the grade of N (not completed), or a letter grade that reflects your course participation. You must officially drop from a course by the necessary deadline in order to remove the course from your transcript. You may also cancel the course and receive a “W” on your transcript and a tuition refund if you meet the refund deadlines for the semester.</p>
         </div>
     );
 };
